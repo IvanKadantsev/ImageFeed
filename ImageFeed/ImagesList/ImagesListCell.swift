5f87aa234051d04d4be5215350ca2071b12ec8d1
IvanKadantsev <ikadancev@yandex.ru>
2026-03-10 19:27:51 +0300

    Добавлена библиотека картинок для отработки таблицы

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+	
+	@IBOutlet var cellImage: UIImageView!
+	@IBOutlet var dateLabel: UILabel!
+	@IBOutlet var likeButton: UIButton!
+	static let reuseIdentifier = "ImagesListCell"
+	
+}

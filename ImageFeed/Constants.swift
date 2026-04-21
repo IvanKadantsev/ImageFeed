185e3d2708d1aca2a9b252645537f3fd4a781675
IvanKadantsev <ikadancev@yandex.ru>
2026-04-21 20:16:21 +0300

    Добавлена авторизация пользователя с сохранением данных авторизации, добавлен splashScreenController с проверкой сохраненного токена

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+enum Constants {
+	static let accessKey = "yTAlRLkXh_bhGVNSnkY_mRYbtFGqstFEFqrIUbvihMk"
+	static let secretKey = "iD1zaPgOlxi6yATORGO1HWRVa5NBxJowMlmnHf8ICgc"
+	static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+	static let accessScope = "public+read_user+write_likes"
+	static let defaultBaseUserString = "https://api.unsplash.com"
+	
+	
+}
+
---
+++ ImageFeed/SplashViewController/SplashViewController.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+protocol AuthViewControllerDelegate: AnyObject {
+	func didAuthenticate(_ vc: AuthViewController)
+}
+
+
+class SplashViewController: UIViewController {
+	
+	private let storage = OAuth2TokenStorage()
+	private let showAuthenticationScreenSegueIdentifier = "showAuth"
+	private let showTabBarController = "showGallery"
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		if storage.token != nil {
+			switchToTabBarController()
+		} else {
+			performSegue(withIdentifier: showAuthenticationScreenSegueIdentifier, sender: nil)
+		}
+	}
+	
+	private func switchToTabBarController() {
+		// Получаем экземпляр `window` приложения
+		guard let window = UIApplication.shared.windows.first else {
+			assertionFailure("Invalid window configuration")
+			return
+		}
+		
+		// Создаём экземпляр нужного контроллера из Storyboard с помощью ранее заданного идентификатора
+		let tabBarController = UIStoryboard(name: "Main", bundle: .main)
+			.instantiateViewController(withIdentifier: "TabBarViewController")
+		   
+		// Установим в `rootViewController` полученный контроллер
+		window.rootViewController = tabBarController
+	}
+}
+
+extension SplashViewController {
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if segue.identifier == showAuthenticationScreenSegueIdentifier {
+			guard
+				let navigationController = segue.destination as? UINavigationController,
+				let viewController = navigationController.viewControllers.first as? AuthViewController
+			else {
+				assertionFailure("Failed to prepare for \(showAuthenticationScreenSegueIdentifier)")
+				return
+			}
+			viewController.delegate = self
+		} else{
+			super.prepare(for: segue, sender: sender)
+		}
+	}
+}
+
+
+extension SplashViewController: AuthViewControllerDelegate {
+	
+
+	
+	func didAuthenticate(_ vc: AuthViewController) {
+		vc.dismiss(animated: true)
+		print("переход к галерее")
+		switchToTabBarController()
+
+	}
+}
